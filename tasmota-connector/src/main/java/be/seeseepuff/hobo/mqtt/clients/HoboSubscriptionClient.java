d284628773150dc0f7c8c6e311ed7a3e8f4e87f4
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2022-10-16 23:02:34 +0200

    Attempting to retry connection to hobo on failure

--- tasmota-connector/src/main/java/be/seeseepuff/hobo/mqtt/clients/HoboSubscriptionClient.java
+++ tasmota-connector/src/main/java/be/seeseepuff/hobo/mqtt/clients/HoboSubscriptionClient.java
@@ -3,13 +3,16 @@ package be.seeseepuff.hobo.mqtt.clients;
 import be.seeseepuff.hobo.graphql.requests.PropertyUpdateFilter;
 import be.seeseepuff.hobo.mqtt.api.HoboApi;
 import be.seeseepuff.hobo.mqtt.services.TasmotaService;
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @ApplicationScoped
 @Slf4j
@@ -24,16 +27,60 @@ public class HoboSubscriptionClient extends AbstractVerticle
 	@ConfigProperty(name = "hobo.owner")
 	String owner;
 
+	private AtomicBoolean keepRunning = new AtomicBoolean(true);
+
+	@SneakyThrows
 	@Override
 	public void start()
 	{
-			log.info("Subscribing to GraphQL update notifications");
-			hoboApi.propertyUpdates(PropertyUpdateFilter.withOwner(owner))
-				.onSubscription().invoke(() -> log.info("Subscribed to changes"))
-				.onItem().invoke(tasmota::onRequest)
-				.onFailure().invoke(ex -> log.error("Exception occurred while processing event", ex))
-				.onFailure().retry().withBackOff(Duration.ofSeconds(5)).indefinitely()
-				.onCompletion().invoke(() -> log.warn("Property requests completed"))
-				.subscribe().with(item -> {});
+		log.info("Subscribing to GraphQL update notifications");
+//		Multi.createFrom()
+//			.items(hoboApi.propertyUpdates(PropertyUpdateFilter.withOwner(owner))
+//				.onSubscription().invoke(() -> log.info("Subscribed to changes"))
+//				.onItem().invoke(tasmota::onRequest)
+//				.onCompletion().invoke(() -> log.warn("Property requests completed"))
+//				.subscribe().asStream())
+//			.onFailure().invoke(ex -> log.error("Exception occurred while processing event", ex))
+//			.onFailure().retry().withBackOff(Duration.ofSeconds(5)).indefinitely()
+//			.subscribe().with(item -> {});
+
+//		Multi.createFrom().item(() -> true)
+//			.onItem().transformToMultiAndConcatenate(item -> hoboApi.propertyUpdates(PropertyUpdateFilter.withOwner(owner)))
+//			//.onFailure().invoke(ex -> log.error("Exception occurred while processing event", ex))
+//			.onFailure().retry().withBackOff(Duration.ofSeconds(5)).indefinitely()
+//			.onCompletion().invoke(() -> log.warn("Property requests completed"))
+//			.subscribe().with(tasmota::onRequest);
+
+//		Multi.createFrom()
+//			.<PropertyUpdate>emitter(emitter -> {
+//				hoboApi.propertyUpdates(PropertyUpdateFilter.withOwner(owner))
+//					.onSubscription().invoke(() -> log.info("Subscribed to changes"))
+////					.onItem().invoke(tasmota::onRequest)
+//					.onFailure().invoke(ex -> log.error("Exception occurred while processing event", ex))
+//					.onFailure().retry().withBackOff(Duration.ofSeconds(5)).indefinitely()
+//					.onFailure().recoverWithMulti(hoboApi.propertyUpdates(PropertyUpdateFilter.withOwner(owner)))
+//					.onCompletion().invoke(() -> log.warn("Property requests completed"))
+//					.subscribe().with(emitter::emit);
+//			})
+//			.onFailure().invoke(ex -> log.error("Exception occurred while processing event", ex))
+//			.onFailure().retry().withBackOff(Duration.ofSeconds(5)).indefinitely()
+//			.subscribe().with(tasmota::onRequest);
+
+		Multi.createFrom().deferred(() -> hoboApi.propertyUpdates(PropertyUpdateFilter.withOwner(owner)))
+			.onSubscription().invoke(() -> log.info("Subscribed to property updates"))
+			.onItem().invoke(tasmota::onRequest)
+			.onCancellation().invoke(() ->
+			{
+				log.info("Cancelling property update subscription");
+				keepRunning.set(false);
+			})
+			.subscribe().with(item -> {});
+	}
+
+	@Override
+	public void stop() throws Exception
+	{
+		Log.info("Stopping property update subscription");
+		keepRunning.set(false);
 	}
 }
